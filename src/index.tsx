eebdd43aaa317e86e14b50a3d772d5fba5fb4cf6
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-03-27 23:30:11 +0200

    add cart route

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./components/navbar"
-import Main from "./pages/main"
+import Navbar from "./components/Navbar"
+import Main from "./pages/Main"
 import { Provider } from "react-redux"
 import store from "./store"
+import Cart from "./pages/Cart"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +18,7 @@ ReactDOM.render(
           <div className="p-5">
             <Routes>
               <Route path="/" element={<Main/>}/>
-              <Route path="cart" element={<Main/>}/>
+              <Route path="cart" element={<Cart/>}/>
             </Routes>
           </div>
         </div>
