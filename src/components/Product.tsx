ebd94f40ac67354d7b8e5771c8ecefdefb9d67c1
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-03-28 22:13:39 +0200

    change link href in product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,7 +2,7 @@ import { ProductInfo } from "../types"
 import React, { FC } from "react"
 import { Link } from "react-router-dom"
 import { useDispatch } from "react-redux"
-import { addToCart } from "../store/cartSlice"
+import { addToCart } from "../store/CartSlice"
 
 const Product: FC<ProductInfo> = (props) => {
   const dispatch = useDispatch()
@@ -12,8 +12,11 @@ const Product: FC<ProductInfo> = (props) => {
   }
 
   return (
-    <div className="p-2 w-full sm:basis-1/2 dark:text-neutral-100">
-      <Link to="/23" className="grid h-full shadow-lg rounded-xl dark:bg-neutral-900 hover:scale-105 transition">
+    <div className="p-2 w-full sm:basis-1/2 dark:text-neutral-100 lg:basis-1/3">
+      <Link
+        to={{pathname: "/product", search: "?id=" + props.id.toString()}}
+        className="grid h-full shadow-lg rounded-xl dark:bg-neutral-900 hover:scale-105 transition"
+      >
         <div className="bg-neutral-200 rounded-t-xl h-56 dark:bg-neutral-700">
           <img
             src={props.image}
@@ -23,13 +26,15 @@ const Product: FC<ProductInfo> = (props) => {
           />
         </div>
         <div className="grid grid-cols-2 grid-rows-2 p-4">
-          <span className="font-bold text-xl">{props.name}</span>
+          <span className="font-bold text-xl truncate">{props.name}</span>
           <span className="text-xl text-right">${props.price.toFixed(2)}</span>
           <span className="col-span-2 truncate">{props.description}</span>
         </div>
         <div className="text-right p-4">
-          <button onClick={handleAddToCart}
-                  className="hover:bg-orange-300 rounded-full w-10 h-10 active:scale-105 active:bg-orange-400 transition">
+          <button
+            onClick={handleAddToCart}
+            className="hover:bg-orange-300 rounded-full w-10 h-10 active:scale-105 active:bg-orange-400 transition"
+          >
             <i className="fa-solid fa-cart-arrow-down text-xl"/>
           </button>
         </div>
