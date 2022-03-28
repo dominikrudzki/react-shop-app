d4f6fe53f152a6d930fc1600868eb8305361390a
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-03-28 22:08:58 +0200

    add products to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import cartSlice from "./CartSlice"
+import productsSlice from "./ProductsSlice"
 
 const store = configureStore({
   reducer: {
+    products: productsSlice,
     cart: cartSlice
   }
 })
