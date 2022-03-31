ae821cd0a6e548a1aabe50443c979196b26d7ffa
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-03-31 19:33:24 +0200

    create tests for removing item

--- src/store/CartSlice.test.tsx
+++ src/store/CartSlice.test.tsx
@@ -1,4 +1,4 @@
-import reducer, { addToCart } from "./CartSlice"
+import reducer, { addToCart, removeFromCart } from "./CartSlice"
 
 describe('CartSlice', () => {
 
@@ -14,7 +14,7 @@ describe('CartSlice', () => {
 		const initialState = {
 			cartItems: [],
 			cartTotalQuantity: 0,
-			cartTotalAmount: 0 // money
+			cartTotalAmount: 0
 		}
 
 		expect(reducer(initialState, addToCart(item))).toEqual({
@@ -28,16 +28,37 @@ describe('CartSlice', () => {
 		const initialState = {
 			cartItems: [{...item, quantity: 1}],
 			cartTotalQuantity: 1,
-			cartTotalAmount: 1 // money
+			cartTotalAmount: 1
 		}
 
-		expect(reducer(initialState, addToCart({...item}))).toEqual({
+		expect(reducer(initialState, addToCart(item))).toEqual({
 			cartItems: [{...item, quantity: 2}],
 			cartTotalQuantity: initialState.cartTotalQuantity + 1,
 			cartTotalAmount: initialState.cartTotalAmount + item.price
 		})
 	})
 
+	test('handle remove item', () => {
+		const item2 = {
+			id: 7,
+			name: 'Hot Dog',
+			image: 'https://cdn.pixabay.com/photo/2012/04/26/10/44/hotdog-42015_960_720.png',
+			description: 'Looks delicious',
+			price: 3.2
+		}
+
+		const initialState = {
+			cartItems: [{...item, quantity: 1}, {...item2, quantity: 1}],
+			cartTotalQuantity: 1,
+			cartTotalAmount: 1
+		}
+
+		expect(reducer(initialState, removeFromCart(item.id))).toEqual({
+			cartItems: [{...item2, quantity: 1}],
+			cartTotalQuantity: initialState.cartTotalQuantity - 1,
+			cartTotalAmount: initialState.cartTotalAmount - item.price
+		})
+	})
 })
 
 export {}
